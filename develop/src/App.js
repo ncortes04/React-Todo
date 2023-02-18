7e5249d6941647c448e06a7afa8001a5be345fe1
Nicholas <nickapoleon@gmail.com>
2023-02-18 11:40:52 -0800

    added sidebar

--- develop/src/App.js
+++ develop/src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
 import {useState} from 'react'
 import './App.css';
-
 function App() {
   const [togglesidebar, setToggleSidebar] = useState(true)
+  const profile = {name: "John Doe", events:{}}
+  const calendar =
+   ['', '', '', '', '',
+  '', '', '', '', '',
+  '', '', '', '', '',
+  '', '', '', '', '',
+  '', '', '', '', '',
+  '','','', '', '', ''
+]
   return (
     <div className="App">
       <nav className="navbar">
@@ -13,23 +21,29 @@ function App() {
         </div>
         <div className="navbar-controls-div">
           <input className="searchbox" placeholder="Search"></input>
-          <button>Days</button>
-          <button>Weeks</button>
-          <button>Month</button>
-          <button>Agenda</button>
-          <button className="profile"></button>
+          <a>Days</a>
+          <a>Weeks</a>
+          <a>Month</a>
+          <a>Agenda</a>
         </div>
+        <button className="profile"></button>
       </nav>
       <main  className="container">
         <aside className={togglesidebar ? "sidebar" : "sidebar hidden"}>
-            <div className="sidebarheader">
-            
+            <div className='profile-div'>
+                <img src={logo} loading="lazy"></img>
+                <p>{profile.name}</p>
             </div>
+            <div className='sidebar-btn-div'><p>Calendar</p></div>
+            <div className='sidebar-btn-div'><p>Events</p></div>
+            <div className='sidebar-btn-div'><p>Taks</p></div>
+            <div className='sidebar-btn-div'><p>Upcoming</p></div>
           </aside>
           <section className="maincontainer">
-
-            <div className="callendarcontainer">
-                
+            <div className="calendarcontainer">
+              {calendar.map((item, index) => {
+                return(<div className='calendar-box'>{index + 1}</div>)
+              })}
             </div>
           </section>
       </main>
